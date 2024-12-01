dcf49b76b2d64dcfdf14c16c8e6a01df17b6533b
hector <hdelgadog94@gmail.com>
2024-12-01 19:43:56 +0100

    Check octagon-app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const [fighters, setFighters] = useState([]);
+ 
+  React.useEffect(() => {
+    axios.get('https://api.octagon-api.com/fighters').then((response) => {
+      setFighters(response.data);
+    });
+
+  }, []);
+  {Object.values(data).map(fighter => fighter.name)};
+  console.log(fighters);
+  <ul>
+  
+   
+  <li key={fighters.name}>{fighters.name}</li>
+
+  </ul>
+  
+  console.log(fighters);
+
+
+};
 
 export default App;
