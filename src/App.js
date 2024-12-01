fb2a799161b997f9f3dd64ad1ab776304374fef3
Lorenzo <lorenzogamboagarcia@gmail.com>
2024-12-01 20:22:26 +0100

    feature: Renders the list of mma fighters

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
@@ -6,25 +5,25 @@ import axios from 'axios';
 
 function App() {
   const [fighters, setFighters] = useState([]);
- 
-  React.useEffect(() => {
+
+  useEffect(() => {
     axios.get('https://api.octagon-api.com/fighters').then((response) => {
       setFighters(response.data);
     });
 
   }, []);
-  {Object.values(data).map(fighter => fighter.name)};
-  console.log(fighters);
-  <ul>
-  
-   
-  <li key={fighters.name}>{fighters.name}</li>
 
-  </ul>
-  
-  console.log(fighters);
-
-
+  return (
+    <div>
+      {Object.values(fighters).map((fighter, id) => {
+        return (
+          <div key={id}>
+            <h1>{fighter.name}</h1>
+          </div>
+        );
+      })};
+    </div>
+  );
 };
 
 export default App;
